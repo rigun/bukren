f8c1367194f02817ab9c697363a0f6f3271c2498
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-11-20 18:21:24 +0700

    fix

--- resources/views/mails/content.blade.php
+++ resources/views/mails/content.blade.php
@@ -4,7 +4,7 @@
             <strong>VERIFIKASI EMAIL DARI <br/>Bukren</strong>
         </div>
         <div class="mail-body" style="color: black; background-color:  #CFE7EA; width: 100%; padding: 20px;">
-            <h1>Hallo {{$dataEmail->penerima}}, Silahkan lakukan verifikasi email anda dengan menekan tombol berikut </h1>
+            <h1>Hallo , Silahkan lakukan verifikasi email anda dengan menekan tombol berikut </h1>
             <a href="https://bukren.thekingcorp.org/verifikasiEmail/{{$dataEmail->token}}"><button style="background-image: linear-gradient(to left, #0025BC , #0071BC); width: 100%; text-align: center; margin: auto; min-height: 40px; color: white; font-size: 30px; cursor: pointer;">Klik disini</button></a>';
         </div>
         <div class="mail-footer" style="color: black; background-color: #adadad; width: 100%; font-size: 20px;padding: 20px; border-bottom-left-radius: 25px; border-bottom-right-radius: 25px;" >';
